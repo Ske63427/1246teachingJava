a01c46fe2d26603545ecea6854c1af03104c9155
email <skelecrae63427@gmail.com>
2024-10-23 08:05:19 -0400

    added example.java to snip code

---
+++ example.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(String[] args) {
+        int x = 5;
+
+        if (x < 10){
+            System.out.println("x is less than 10");
+        } else if (x == 10){
+            System.out.println("x is equal to 10");
+        } else {
+            System.out.println("x is greater than 10");
+        }
+    }
+}
+
